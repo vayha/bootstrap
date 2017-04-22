11c50d555d32cdec0c4ab70f538538d753d20dde
Mateusz Wegrzyn <matiice@interia.pl>
2017-04-22 13:40:59 +0200

    podstawy end

--- js/boot.js
+++ js/boot.js
@@ -1,55 +1,7 @@
 'use strict'
 
 
+$('.carousel').carousel({
+  interval: 1000
+})
 
-var i;
-var tablicaFibo = [];
-
-function poLicz(liczba) {
-    var suma = 0;
-   
-
- for (i =0; i <=liczba; i++) {
-     
-     if (i==0){
-         tablicaFibo[i] = 0;
-         
-     }
-     
-     else if (i==1){
-         tablicaFibo[i] = 1;
-     } 
-     
-     else {
-         tablicaFibo[i] = tablicaFibo[i-1] + tablicaFibo[i-2];
-     }
-     
-     suma += tablicaFibo[i];
-     
-    
-     
- }
-    console.log(suma);
-   
-}
-
-poLicz(4);
-
-/*----------------------------------*/
-
-var total = 0;
-var count = 1;
-while (count <= 10) {
-  total += count;
-  count += 1;
-}
-console.log(total);
-// → 55
-
-var i;
-var suma = 0;
-for (i=0; i<=10; i++)
-    {
-     suma +=i;
-     console.log(suma);
-    }
